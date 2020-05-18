3a5959a35e34f174dd521cae9b8089a7b00c7773
yuji91 <yuji91.1227@gmail.com>
2020-05-18 12:04:04 +0900

    rails g controller users new show create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
   get 'static_pages/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
