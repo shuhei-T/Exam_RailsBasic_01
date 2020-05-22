b34a8eb20b5daca332bc620cfd8903909322f4da
KentarouTawara <sironagasuken2@gmail.com>
2020-05-22 15:16:23 +0900

    add better_errors, binding_of_caller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user.authenticate(params[:session][:password])
-      log_in user
+      login user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
